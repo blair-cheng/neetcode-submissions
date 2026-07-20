3154aff44ed40916f5d14aba472679f24a60311a
Blair <95343554+blair-cheng@users.noreply.github.com>
2026-07-19 20:42:47 -0700

    Add: house-robber-ii - submission-18

---
+++ Data Structures & Algorithms/house-robber-ii/submission-18.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0],self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        n = len(nums)
+        dp = [0 for i in range(n + 2)]
+        # let dp[i] be the total money one can rob from house i to n
+
+        for i in range(n-1, -1, -1):
+            dp[i] = max(dp[i+1], nums[i] + dp[i+2] )
+        return dp[0]
